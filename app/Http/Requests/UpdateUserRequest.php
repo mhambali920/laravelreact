a6e2e16b1a11ef92c96c63f23e5bd8aecea82a72
mhambali920 <mhambali920@gmail.com>
2023-06-09 14:03:13 +0700

    update updateUserRequest

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -27,7 +27,7 @@ class UpdateUserRequest extends FormRequest
         return [
             'name' => 'required|string|max:255',
             'email' => [
-                'required', 'eamil', Rule::unique('users', 'email')->ignore($this->user)
+                'required', 'email', Rule::unique('users', 'email')->ignore($this->id)
             ],
             'password' => [
                 'required',
